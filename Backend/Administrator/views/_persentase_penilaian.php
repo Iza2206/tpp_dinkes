e49f028620b408341ac97a523e884cd92c470796
Iza2206 <izatunnisa06@gmail.com>
2024-03-23 10:40:49 +0700

    update proses bayar dan laporan

---
+++ Backend/Administrator/views/_persentase_penilaian.php
@@ -0,0 +1,306 @@
+<div class="pagetitle">
+    <h1>Rekepitulasi Persentase Penilaian Aspek Perilaku dan Prestasi Kerja (Lampiran V)</h1>
+</div>
+
+<!-- End Page Title -->
+<section class="section">
+    <div class="row">
+        <div class="col-lg-12 ">
+            <div class="card">
+                <div class="card-body">
+                    <div class="mt-3">
+                        <form>
+                            <div class="d-flex align-items-center mb-3">
+                                <h5 style="margin-right: 10px; margin-bottom: 0;">Unit Kerja</h5>
+                                <select class="form-select" style="width: 200px; margin-bottom: 0;">
+                                <option selected disabled>Pilih Unit Kerja</option>
+                                <option value="1">(1) DINAS KESEHATAN</option>
+                                <option value="2">(2) RSU BANGUN PURBA</option>
+                                <option value="3">(3) RSU PANCUR BATU</option>
+                                <option value="4">(4) RSUD H. AMRI TAMBUNAN</option>
+                                <option value="5">(5) UPT. PUSKESMAS ARAS KABU</option>
+                                <option value="6">(6) UPT. PUSKESMAS BANDAR KHALIPAH KECAMATAN PERCUT SEI TUAN</option>
+                                <option value="7">(7) UPT. PUSKESMAS BANDAR BARU KABUPATEN DELI SERDANG</option>
+                                <option value="8">(8) UPT. PUSKESMAS BANGUN PURBA KABUPATEN DELI SERDANG</option>
+                                <option value="9">(9) UPT. PUSKESMAS BATANG KUIS KEC. BATANG KUIS</option>
+                                <option value="10">(10) UPT. PUSKESMAS BIRU - BIRU KECAMATAN BIRU -BIRU</option>
+                                <option value="11">(11) UPT. PUSKESMAS DALU SEPULUH KEC. TANJUNG MORAWA</option>
+                                <option value="12">(12) UPT. PUSKESMAS DELI TUA KECAMATAN DELI TUA</option>
+                                <option value="13">(13) UPT. PUSKESMAS GALANG KEC. GALANG</option>
+                                <option value="14">(14) UPT. PUSKESMAS GUNUNG MERIAH KEC. GUNUNG MERIAH</option>
+                                <option value="15">(15) UPT. PUSKESMAS GUNUNG TINGGI KABUPATEN DELI SERDANG</option>
+                                <option value="16">(16) UPT. PUSKESMAS HAMPARAN PERAK KEC. HAMPARAN PERAK</option>
+                                <option value="17">(17) UPT. PUSKESMAS KARANG ANYAR</option>
+                                <option value="18">(18) UPT. PUSKESMAS KENANGAN KECAMATAN PERCUT SEI TUAN</option>
+                                <option value="19">(19) UPT. PUSKESMAS KOTA DATAR KECAMATAN HAMPARAN PERAK</option>
+                                <option value="20">(20) UPT. PUSKESMAS KUTALIMBARU</option>
+                                <option value="21">(21) UPT. PUSKESMAS LABUHAN DELI KECAMATAN LABUHAN</option>
+                                <option value="22">(22) UPT. PUSKESMAS LUBUK PAKAM KEC. LUBUK PAKAM</option>
+                                <option value="23">(23) UPT. PUSKESMAS MULYOREJO KEC. SUNGGAL</option>
+                                <option value="24">(24) UPT. PUSKESMAS NAMORAMBE KABUPATEN DELI SERDANG</option>
+                                <option value="25">(25) UPT. PUSKESMAS PAGAR JATI KEC. LUBUK PAKAM</option>
+                                <option value="26">(26) UPT. PUSKESMAS PAGAR MERBAU KEC. PAGAR MERBAU</option>
+                                <option value="27">(27) UPT. PUSKESMAS PANCUR BATU KABUPATEN DELI SERDANG</option>
+                                <option value="28">(28) UPT. PUSKESMAS PANTAI LABU KEC. PANTAI LABU</option>
+                                <option value="29">(29) UPT. PUSKESMAS PATUMBAK KECAMATAN PATUMBAK</option>
+                                <option value="30">(30) UPT. PUSKESMAS PEMATANG JOHAR KEC. LABUHAN DELI</option>
+                                <option value="31">(31) UPT. PUSKESMAS PETUMBUKAN KECAMATAN GALANG</option>
+                                <option value="32">(32) UPT. PUSKESMAS SEI SEMAYANG KECAMATAN SUNGGAL</option>
+                                <option value="33">(33) UPT. PUSKESMAS SEI MENCIRIM KABUPATEN DELI SERDANG</option>
+                                <option value="34">(34) UPT. PUSKESMAS SIALANG</option>
+                                <option value="35">(35) UPT. PUSKESMAS SIBOLANGIT KEC. SIBOLANGIT</option>
+                                <option value="36">(36) UPT. PUSKESMAS TALUN KENAS KABUPATEN DELI SERDANG</option>
+                                    <!-- Opsi Unit Kerja -->
+                                </select>
+                            </div>
+                            <div class="d-flex align-items-center mb-3">
+                                <h5 style="margin-right: 40px; margin-bottom: 0;">Bulan</h5>
+                                <select class="form-select" style="width: 200px; margin-bottom: 0;">
+                                <option selected disabled>Pilih Bulan</option>
+                                <option value="1">(1) Januari</option>
+                                <option value="2">(2) Februari</option>
+                                <option value="3">(3) Maret</option>
+                                <option value="4">(4) April</option>
+                                <option value="5">(5) Mei</option>
+                                <option value="6">(6) Juni</option>
+                                <option value="7">(7) Juli</option>
+                                <option value="8">(8) Agustus</option>
+                                <option value="9">(9) September</option>
+                                <option value="10">(10) Oktober</option>
+                                <option value="11">(11) November</option>
+                                <option value="12">(12) Desember</option>
+                                <option value="13">(13) TPP THR</option>
+                                <option value="14">(14) TPP 13</option>
+                                    <!-- Opsi Bulan -->
+                                </select>
+                            </div>
+                            <div class="d-flex align-items-center mb-3">
+                                <h5 style="margin-right: 40px; margin-bottom: 0;">Tahun</h5>
+                                <select class="form-select" style="width: 200px; margin-bottom: 0;">
+                                    <option selected disabled>Pilih Tahun</option>
+                                    <option value="1">2023</option>
+                                    <option value="2">2022</option>
+                                    <option value="3">2020</option>
+                                        <!-- Opsi Tahun -->
+                                </select>
+                            </div>
+                            <div class="d-flex align-items-center mb-3">
+                                <button type="submit" name="submit" value="Submit" class="btn btn-primary btn-md">Submit</button>
+                            </div>
+                        </form>
+                    </div>
+
+                    <!-- End Large Modal-->
+                    <!-- Table with stripped rows -->
+                    <div class="table-responsive">
+                        <div class="btn-group float-right">
+                            <button type="button" onclick="exportToExcel('myTable')" class="btn btn-primary btn-md">Export to Excel</button>
+                            <a href="../Administrator/dashboard_blank.php?page=Print_all" target="_blank" class="btn btn-success btn-md" style="margin-left: 10px;">Export PDF</a>
+                        </div>
+                        <table id="myTable" class="table table-bordered table-striped">
+                            <thead>
+                                <tr>
+                                    <!-- Column headers -->
+                                    <th scope="col" rowspan="2" style="text-align: center;">no</th> 
+                                    <th scope="col" rowspan="2" style="text-align: center;">Nama</th> 
+                                    <th scope="col" rowspan="2" style="text-align: center;">NIP</th> 
+                                    <th scope="col" rowspan="2" style="text-align: center;">GOL</th> 
+                                    <th scope="col" rowspan="2" style="text-align: center;">JABATAN</th> 
+                                    <th scope="col" style="text-align: center;"> ASPEK PERILAKU KERJA</th> 
+                                    <th scope="col" colspan="2" style="text-align: center;">ASPEK PRESTASI KERJA</th> 
+                                    <th scope="col" rowspan="2" style="text-align: center;">JUMLAH <br> TL1-4(%)</th> 
+                                    <th scope="col" rowspan="2" style="text-align: center;">JUMLAH <br> PSW1-4(%)</th> 
+                                    <th scope="col" rowspan="2" style="text-align: center;">TOTAL PERSENTASE PENILAIAN</th> 
+                                    <th scope="col" rowspan="2" style="text-align: center;">KET</th> 
+                                </tr>
+
+                                <tr>
+                                    <!-- sub Kompenen perhitungan -->
+                                    <th scope="col" style="text-align: center;">Kehadiran</th>
+                                    <th scope="col" style="text-align: center;">SKP</th>
+                                    <th scope="col" style="text-align: center;">Lap.Harian</th>
+                                </tr>
+                                <tr>
+                                    <!-- Column headers -->
+                                    <th scope="col"style="text-align: center;">1</th> 
+                                    <th scope="col"style="text-align: center;">2</th> 
+                                    <th scope="col"style="text-align: center;">3</th> 
+                                    <th scope="col"style="text-align: center;">4</th> 
+                                    <th scope="col"style="text-align: center;">5</th> 
+                                    <th scope="col"style="text-align: center;">6</th> 
+                                    <th scope="col"style="text-align: center;">7</th> 
+                                    <th scope="col"style="text-align: center;">8</th> 
+                                    <th scope="col"style="text-align: center;">9</th> 
+                                    <th scope="col"style="text-align: center;">10</th> 
+                                    <th scope="col"style="text-align: center;">11</th> 
+                                    <th scope="col"style="text-align: center;">12</th> 
+                                </tr>
+                            </thead>
+                            <tbody>
+                                    <td>1</td>
+                                    <td>Oman</td>
+                                    <td>2123132123</td>
+                                    <td>IV/e</td>
+                                    <td>test</td>
+                                    <td>60.00</td>
+                                    <td>20.00</td>
+                                    <td>20.00</td>
+                                    <td>00.00</td>
+                                    <td>00.00</td>
+                                    <td>100%</td>
+                                    <td></td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+
+<script>
+    function exportToExcel(tableId) {
+        // Ambil elemen tabel berdasarkan ID
+        var table = document.getElementById(tableId);
+        var rows = table.querySelectorAll('tr');
+      
+        // Buat objek Excel dengan format yang sesuai
+        var excel = '<html xmlns:o="urn:schemas-microsoft-com:office:office" xmlns:x="urn:schemas-microsoft-com:office:excel" xmlns="http://www.w3.org/TR/REC-html40">';
+        excel += '<head><meta charset="UTF-8"></head><body>';
+
+        excel += '<style>';
+        excel += 'h5 { margin: 0; padding: 0; font-weight: normal;}'; // Mengatur margin dan padding menjadi 0 untuk tag h5
+        excel += 'table { border-collapse: collapse; width: 100%; }'; // Menambahkan aturan CSS untuk tabel
+        excel += 'td, th { border: 1px solid black; padding: 5px; }'; // Menambahkan aturan CSS untuk sel-sel tabel
+        excel += '</style>';
+
+        excel += '<h5>LAMPIRAN V</h5>';
+        excel += '<h5>PERATURAN BUPATI DELI SERDANG</h5>';
+        excel += '<h5>NOMOR 04 TAHUN 2019</h5>';
+        excel += '<h5>TENTANG</h5>';
+        excel += '<h5>TAMBAHAN PENGHASILAN PEGAWAI NEGERI SIPIL DAERAH</h5>';
+        excel += '<h5>KABUPATEN DELI SERDANG</h5>';
+        // Tambahkan judul tabel dan bulan
+        excel += '<h3 style="text-align: center;">REKAPITULASI PERSENTASE PENILAIAN ASPEK PERILAKU KERJA DAN ASPEK PRESTASI KERJA PNSD<br>';
+        excel += 'DINAS KESEHATAN KABUPATEN DELI SERDANG<br>';
+        excel += 'DINAS KESEHATAN</h3>';
+
+        excel += '<h5><b>BULAN: JANUARI 2023</b></h5>';
+
+        excel += '<table>';
+        for (var i = 0; i < rows.length; i++) {
+            excel += rows[i].outerHTML;
+        }
+        excel += '</table>';
+
+
+        excel += '<table style="border-collapse: collapse; font-family: Arial Narrow; font-size: 8pt; width: 100%;">';
+        excel += '<tr>';
+        excel += '<td colspan="12" style="border: none;"></td>'; // Kolom 1, Baris 1-19 kosong
+        excel += '</tr>';
+        excel += '<tr>';
+        excel += '<td colspan="1" style="border: none;"></td>'; // Kolom 1-
+        excel += '<td colspan="3" style="border: none;">TELAH DIVERIFIKASI OLEH</td>'; // Kolom 4-12, Baris 1
+        excel += '<td colspan="4" style="border: none;"></td>'; // Kolom 4-12, Baris 1
+        excel += '<td colspan="3" style="border: none;">Lubuk Pakam,</td>'; // Kolom 4-12, Baris 1
+        excel += '</tr>';
+        excel += '<tr>';
+        excel += '<td colspan="1" style="border: none;"></td>'; // Kolom 1-3, Baris 1-19 kosong
+        excel += '<td colspan="3" style="border: none;">Tanggal</td>'; // Kolom 4-12, Baris 2
+        excel += '</tr>';
+        excel += '<tr>';
+        excel += '<td colspan="12" style="border: none;"></td>'; // Kolom 1, Baris 1-19 kosong
+        excel += '</tr>';
+        excel += '<tr>';
+        excel += '<td colspan="12" style="border: none;"></td>'; // Kolom 1, Baris 1-19 kosong
+        excel += '</tr>';
+        excel += '<tr>';
+        excel += '<td colspan="1" style="border: none;"></td>'; // Kolom 1-3, Baris 1-19 kosong
+        excel += '<td colspan="3" style="border: none;">Ka. Sub. Bag. Hukum, Kepegawaian dan Umum</td>'; // Kolom 4-12, Baris 5
+        excel += '<td colspan="4" style="border: none;"></td>'; // Kolom 4-12, Baris 1
+        excel += '<td colspan="3" style="border: none;">Sekretaris Dinas Kesehatan</td>'; // Kolom 4-12, Baris 1
+        excel += '</tr>';
+        excel += '<tr>';
+        excel += '<td colspan="1" style="border: none;"></td>'; // Kolom 1-3, Baris 1-19 kosong
+        excel += '<td colspan="3" style="border: none;">Dinas Kesehatan Kabupaten Deli Serdang</td>'; // Kolom 4-12, Baris 6
+        excel += '<td colspan="4" style="border: none;"></td>'; // Kolom 4-12, Baris 1
+        excel += '<td colspan="3" style="border: none;">Dinas Kesehatan Kabupaten Deli Serdang</td>'; // Kolom 4-12, Baris 1
+        excel += '</tr>';
+        excel += '<tr>';
+        excel += '<td colspan="12" style="border: none;"></td>'; // Kolom 1, Baris 1-19 kosong
+        excel += '</tr>';
+        excel += '<tr>';
+        excel += '<td colspan="12" style="border: none;"></td>'; // Kolom 1, Baris 1-19 kosong
+        excel += '</tr>';
+        excel += '<tr>';
+        excel += '<td colspan="1" style="border: none;"></td>'; // Kolom 1-3, Baris 1-19 kosong
+        excel += '<td colspan="3" style="border: none;"><u>Sri Rezeki</u></td>'; // Kolom 4-12, Baris 10
+        excel += '<td colspan="4" style="border: none;"></td>'; // Kolom 4-12, Baris 1
+        excel += '<td colspan="3" style="border: none;"><u>dr. Tetty Rossanti Keliath</u></td>'; // Kolom 4-12, Baris 1
+        excel += '</tr>';
+        excel += '<tr>';
+        excel += '<td colspan="1" style="border: none;"></td>'; // Kolom 1-3, Baris 1-19 kosong
+        excel += '<td colspan="3" style="border: none;">NIP. 19710712 199303 1009</td>'; // Kolom 4-12, Baris 11
+        excel += '<td colspan="4" style="border: none;"></td>'; // Kolom 4-12, Baris 1
+        excel += '<td colspan="3" style="border: none;">NIP. 19770418 200312 2 009</td>'; // Kolom 4-12, Baris 1
+        excel += '</tr>';
+        excel += '<tr>';
+        excel += '<td colspan="12" style="border: none;"></td>'; // Kolom 1, Baris 1-19 kosong
+        excel += '</tr>';
+        excel += '<tr>';
+        excel += '<td colspan="1" style="border: none;"></td>'; // Kolom 1-3, Baris 1-19 kosong
+        excel += '<td colspan="3" style="border: none;">Pejabat di Bidang Pemberhentian dan Pensiun BKD</td>'; // Kolom 4-12, Baris 14
+        excel += '</tr>';
+        excel += '<tr>';
+        excel += '<td colspan="12" style="border: none;"></td>'; // Kolom 1, Baris 1-19 kosong
+        excel += '</tr>';
+        excel += '<tr>';
+        excel += '<td colspan="12" style="border: none;"></td>'; // Kolom 1, Baris 1-19 kosong
+        excel += '</tr>';
+        excel += '<tr>';
+        excel += '<td colspan="1" style="border: none;"></td>'; // Kolom 1-3, Baris 1-19 kosong
+        excel += '<td colspan="12" style="border: none;">NIP.</td>'; // Kolom 1-12, Baris 18
+        excel += '</tr>';
+        excel += '<tr>';
+        excel += '<td colspan="12" style="border: none;"></td>'; // Kolom 1, Baris 1-19 kosong
+        excel += '</tr>';
+        excel += '<tr>';
+        excel += '<td colspan="1" style="border: none;"></td>'; // Kolom 1-3, Baris 1-19 kosong
+        excel += '<td colspan="3" style="border: none;">Pejabat di Bidang Penilaian Kinerja Aparatur dan Promosi BKD</td>'; // Kolom 4-12, Baris 19
+        excel += '</tr>';
+        excel += '<tr>';
+        excel += '<td colspan="12" style="border: none;"></td>'; // Kolom 1, Baris 1-19 kosong
+        excel += '</tr>';
+        excel += '<tr>';
+        excel += '<td colspan="12" style="border: none;"></td>'; // Kolom 1, Baris 1-19 kosong
+        excel += '</tr>';
+        excel += '<tr>';
+        excel += '<td colspan="1" style="border: none;"></td>'; // Kolom 1-3, Baris 1-19 kosong
+        excel += '<td colspan="12" style="border: none;">NIP.</td>'; // Kolom 1-12, Baris 23
+        excel += '</tr>';
+        excel += '</table>';
+
+        
+        // Tutup tag HTML
+        excel += '</body></html>';
+
+        // Buat blob dari objek Excel
+        var blob = new Blob([excel], { type: 'application/vnd.ms-excel' });
+        
+        // Buat URL dari blob
+        var url = URL.createObjectURL(blob);
+        
+        // Buat link untuk download file Excel
+        var a = document.createElement('a');
+        a.href = url;
+        a.download = 'exported_data.xls'; // Nama file Excel yang akan diunduh
+        document.body.appendChild(a);
+        a.click();
+        document.body.removeChild(a);
+    }
+</script>
+<!-- <script>
+
+</script> -->
---
+++ Backend/Administrator/views/_print_all.php
@@ -0,0 +1,235 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>persentase</title>
+    <style>
+    /* table {
+            width: 100%;
+            border-collapse: collapse;
+            } */
+
+    th,
+    td {
+        border: 1px solid black;
+        padding: 8px;
+        text-align: left;
+    }
+    
+    h2 {
+        /* width: 40rem; */
+        padding: 10px;
+        margin: 0 auto;
+        margin-bottom: 10px;
+        text-align: center;
+        font-family: arial;
+        font-size: 10pt;
+        /* max-width: 40rem; */
+    }
+
+    .ttd {
+        border-top: none;
+        border-bottom: none;
+        border-left: none;
+        border-right: none;
+    }
+
+    
+
+    h5 {
+            margin: auto;
+            font-size: 6pt;
+            padding: 1px;
+            font-family: arial;
+            
+        }
+
+    th {
+        background-color: #f2f2f2;
+        font-weight: bold;
+    }
+
+    tr:nth-child(even) {
+        background-color: #f2f2f2;
+    }
+
+    @media screen and (max-width: 900px) {
+    /* table, */
+    thead,
+    tbody,
+    th,
+    td,
+        tr {
+            display: block;
+        }
+
+        th,
+        td {
+            padding: 8px;
+        }
+
+        th {
+            height: 40px;
+        }
+
+        tr {
+            margin: 0 0 16px 0;
+        }
+
+        
+    }
+    </style>
+</head>
+<body>
+
+    <h5>
+        LAMPIRAN V
+    </h5>
+    <h5>
+        PERATURAN BUPATI DELI SERDANG
+    </h5>
+    <h5>
+        NOMOR 04 TAHUN 2019
+    </h5>
+    <h5>
+        TENTANG
+    </h5>
+    <h5>
+        TAMBAHAN PENGHASILAN PEGAWAI NEGERI SIPIL DAERAH
+    </h5>
+    <h5>
+        KABUPATEN DELI SERDANG
+    </h5>
+
+    <h2>REKAPITULASI PERSENTASE PENILAIAN ASPEK PERILAKU KERJA DAN ASPEK PRESTASI KERJA PNSD<br>
+        DINAS KESEHATAN KABUPATEN DELI SERDANG<br>
+        DINAS KESEHATAN</h2>
+
+    <h5>
+        BULAN: JANUARI 2023
+    </h5>
+    <table style="border: 1px solid black; border-collapse: collapse; font-family: arial; font-size: 8pt; max-width: 900px;margin-bottom: 10px;">
+        <tbody>
+        <tr>
+            <td rowspan="2">NO</td>
+            <td rowspan="2" style="width: 60%;text-align: center;">NAMA</td>
+            <td rowspan="2" style="width: 5%;text-align: center;">NIP</td>
+            <td rowspan="2" style="width: 4px; text-align: center;">GOL</td>
+            <td rowspan="2" style="text-align: center;width: 5%;">JABATAN</td>
+            <td style="padding: 0%; text-align: center;">ASPEK PERILAKU KERJA</td>
+            <td colspan="2" style="text-align: center;">ASPEK PRESTASI KERJA</td>
+            <td rowspan="2" style="text-align: center;">JUMLAH TL1-4(%)</td>
+            <td rowspan="2" style="text-align: center;">JUMLAH PSW1-4(%)</td>
+            <td rowspan="2" style="text-align: center;">TOTAL PERSENTASE PENILAIAN</td>
+            <td rowspan="2" style="text-align: center;">KET</td>
+        </tr>
+        <tr>
+            <td style="text-align: center;padding:1;">Kehadiran %</td>
+            <td style="text-align: center;">SKP %</td>
+            <td style="text-align: center;">Lap. Harian %</td>
+        </tr>
+        <tr>
+            <td style="text-align: center; width: 2px;padding: 0%;">1</td>
+            <td style="text-align: left;width: 10px; padding: 1">Isi namanya disini Lorem ipsum </td>
+            <td style="text-align: left;padding: 1">12345122321213123</td>
+            <td style="text-align: left;padding: 1">iv/c </td>
+            <td style="text-align: left;padding: 1">Jabatannya oke </td>
+            <td style="text-align: left;padding: 1">Kehadiran nya </td>
+            <td style="text-align: left;padding: 1">SKP uyy</td>
+            <td style="text-align: left;padding: 1">Lap. Harian ini </td>
+            <td style="text-align: left;padding: 1">Jumlah tl ini </td>
+            <td style="text-align: left;padding: 1">Jumlah psw ini </td>
+            <td style="text-align: left;padding: 1">Total persentase </td>
+            
+
+        </tr>
+        </tbody>
+
+        </table>
+    
+        <table style="border-collapse: collapse; font-family: Arial Narrow; font-size: 8pt; width: 100%;">
+        <tr>
+        <td colspan="1" style="border: none;"></td> 
+        <td colspan="3" style="border: none;">TELAH DIVERIFIKASI OLEH</td> 
+        <td colspan="4" style="border: none;"></td> 
+        <td colspan="3" style="border: none;">Lubuk Pakam,</td> 
+        </tr>
+        <tr>
+        <td colspan="1" style="border: none;"></td>
+        <td colspan="3" style="border: none;">Tanggal</td> 
+        </tr>
+        <tr>
+        <td colspan="12" style="border: none;"></td> 
+        </tr>
+        <tr>
+        <td colspan="12" style="border: none;"></td> 
+        </tr>
+        <tr>
+        <td colspan="1" style="border: none;"></td>
+        <td colspan="3" style="border: none;">Ka. Sub. Bag. Hukum, Kepegawaian dan Umum</td> 
+        <td colspan="4" style="border: none;"></td> 
+        <td colspan="3" style="border: none;">Sekretaris Dinas Kesehatan</td> 
+        </tr>
+        <tr>
+        <td colspan="1" style="border: none;"></td>
+        <td colspan="3" style="border: none;">Dinas Kesehatan Kabupaten Deli Serdang</td>
+        <td colspan="4" style="border: none;"></td> 
+        <td colspan="3" style="border: none;">Dinas Kesehatan Kabupaten Deli Serdang</td> 
+        </tr>
+        <tr>
+        <td colspan="12" style="border: none;"></td> 
+        </tr>
+        <tr>
+        <td colspan="12" style="border: none;"></td> 
+        </tr>
+        <tr>
+        <td colspan="1" style="border: none;"></td>
+        <td colspan="3" style="border: none;"><u>Sri Rezeki</u></td>
+        <td colspan="4" style="border: none;"></td> 
+        <td colspan="3" style="border: none;"><u>dr. Tetty Rossanti Keliath</u></td> 
+        </tr>
+        <tr>
+        <td colspan="1" style="border: none;"></td>
+        <td colspan="3" style="border: none;">NIP. 19710712 199303 1009</td>
+        <td colspan="4" style="border: none;"></td> 
+        <td colspan="3" style="border: none;">NIP. 19770418 200312 2 009</td> 
+        </tr>
+        <tr>
+        <td colspan="1" style="border: none;"></td>
+        <td colspan="3" style="border: none;">Pejabat di Bidang Pemberhentian dan Pensiun BKD</td>
+        </tr>
+        <tr>
+        <td colspan="12" style="border: none;"></td> 
+        </tr>
+        <tr>
+        <td colspan="12" style="border: none;"></td> 
+        </tr>
+        <tr>
+        <td colspan="1" style="border: none;"></td>
+        <td colspan="12" style="border: none;">NIP.</td>
+        </tr>
+        <tr>
+        <td colspan="1" style="border: none;"></td>
+        <td colspan="3" style="border: none;">Pejabat di Bidang Penilaian Kinerja Aparatur dan Promosi BKD</td>
+        </tr>
+        <tr>
+        <td colspan="12" style="border: none;"></td> 
+        </tr>
+        <tr>
+        <td colspan="12" style="border: none;"></td> 
+        </tr>
+        <tr>
+        <td colspan="1" style="border: none;"></td>
+        <td colspan="12" style="border: none;">NIP.</td>
+        </tr>
+        </table>
+
+    
+</body>
+</html>
+
+<script type="text/javascript">
+    window.print();
+</script>
+ 
